4b5716a3a30422ceb19108073d767eb6923f0eff
YeongsuKimm <yeongsuk505@gmail.com>
2025-01-27 03:28:22 -0800

    100/100

--- data/audio_conv.py
+++ data/audio_conv.py
@@ -2,7 +2,7 @@ import ffmpeg
 import os
 
 input_folder = "zzz.videos"
-output_folder = "Fannsy"
+output_folder = "LanceMcDonald"
 
 # Ensure the output folder exists
 if not os.path.exists(output_folder):
